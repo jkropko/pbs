1be2d62dc866815c835c85fd44e9a980a23e43f2
jkropko <jkropko@virginia.edu>
2023-07-14 14:03:43 +0000

    adding first analysis markdown file

---
+++ DataCleaning.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(readxl)
+
+#load the raw data
+data <-read_excel("Copy of PBS+and+Trust+in+America_+National+PBS+Audience+Survey_January+25,+2021_08.08.xlsx")
+
+#change column names
+#delete row 1
+#JK is awesome! 
+
+code coe code
